434d61196c8cd920b8672f212c2ead1a5a345fbc
JangYW <wkddyddns6@gmail.com>
2025-07-17 14:10:03 +0900

    🎉 배포용 수정 완료

--- app/(movies)/movies/[id]/page.tsx
+++ app/(movies)/movies/[id]/page.tsx
@@ -2,28 +2,18 @@ import MovieInfo from "@/components/movie-info";
 import MovieVideos from "@/components/movie-videos";
 import { Suspense } from "react";
 
-// import { API_URL } from "@/lib/constants";
-// async function getMovies(){
-// 	// await new Promise((resolve => setTimeout(resolve, 1000)))
-// 	const response = await fetch (API_URL);
-// 	const json = await response.json();
-// 	return json;
-// }
+// ✅ 타입 강제 없이 함수에서 바로 구조분해
+export default function MovieDetail({ params }: { params: { id: string } }) {
+  const { id } = params;
 
-export default async function MovieDetail({
-	params: { id },
-}: {
-	params: { id: string };
-}) {
-	// const movie = await getMovies(); // ✅ 이거 하나만 있어도 됨!
-	return (
-	<div>
-		<Suspense fallback = {<h1>Loading movie Info</h1>} >
-			<MovieInfo id = {id}></MovieInfo>
-		</Suspense>
-		<Suspense fallback = {<h1>Loading movie Video</h1>} >
-			<MovieVideos id = {id} ></MovieVideos>		
-		</Suspense>			
-	</div>
-	);
-} 
+  return (
+    <div>
+      <Suspense fallback={<h1>Loading movie Info</h1>}>
+        <MovieInfo id={id} />
+      </Suspense>
+      <Suspense fallback={<h1>Loading movie Video</h1>}>
+        <MovieVideos id={id} />
+      </Suspense>
+    </div>
+  );
+}
